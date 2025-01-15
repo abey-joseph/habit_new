974140081a58dd6b7375e24f86d0590307724d95
Abey Joseph <abeyna567@gmail.com>
2025-01-15 10:39:03 +0800

    initialize

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(HabitTracker());
+}
+
+class HabitTracker extends StatelessWidget {
+  const HabitTracker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
