120b8ab87989557da65f830b1146260eea1e1549
Abey Joseph <abeyna567@gmail.com>
2025-01-31 23:23:02 +0800

    done

--- lib/core/bloc/detail_bloc/detail_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'detail_bloc.dart';
-
-@freezed
-class DetailEvent with _$DetailEvent {
-  const factory DetailEvent.fetchHabitDetail({required int index}) =
-      fetchHabitDetail;
-  const factory DetailEvent.refreshHabitDetail() = refreshHabitDetail;
-}
--- lib/data/constant/last_5_days/last_5_days_list.dart
+++
@@ -1,7 +0,0 @@
-List<DateTime> lastFiveDays = [];
-
-// A function to generate a list of dates starting from today
-List<DateTime> getLastFiveDays() {
-  DateTime today = DateTime.now();
-  return List.generate(5, (index) => today.subtract(Duration(days: index)));
-}
